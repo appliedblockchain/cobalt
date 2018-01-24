e152214e2c52045227e53d4f4a33728e19d23d1a
Mirek Rusin <mirek@me.com>
2018-01-24 18:02:28 +0000

    Pass null when there's no compiled bytecode.

--- solc.js
+++ solc.js
@@ -27,7 +27,7 @@ function make({ root = DEFAULT_ROOT, solc = DEFAULT_SOLC } = {}) {
     const parsed = JSON.parse(stdout)
     const r = {}
     for (const [key, { bin, abi }] of Object.entries(parsed.contracts)) {
-      r[key.split(':')[1]] = { abi: JSON.parse(abi), bin: '0x' + bin }
+      r[key.split(':')[1]] = { abi: JSON.parse(abi), bin: bin ? '0x' + bin : null }
     }
     return r
   }
