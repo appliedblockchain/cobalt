54b84296a4cc1d7d5274e95a482c704b9991bc31
Mirek Rusin <mirek@me.com>
2018-03-08 15:25:14 +0000

    Updating `derive(...)` to auto-generate private key if not provided.

--- derive.js
+++ derive.js
@@ -2,21 +2,27 @@
 const secp256k1 = require('secp256k1')
 const toChecksumAddress = require('./to-checksum-address')
 const keccak256 = require('./keccak256')
+const makePrivateKey = require('./make-private-key')
 
-function derive(privateKey) {
+function derive(maybePrivateKey) {
+
+  const privateKey = maybePrivateKey ? maybePrivateKey : makePrivateKey()
 
   if (!secp256k1.privateKeyVerify(privateKey)) {
     throw new TypeError('Invalid private key.')
   }
 
-  const publicKey = secp256k1.publicKeyCreate(privateKey, false).slice(1)
+  const publicKeyBuffer = secp256k1.publicKeyCreate(privateKey, false).slice(1)
+  const address = toChecksumAddress(keccak256(publicKeyBuffer).slice(-20))
+  const publicKey = '0x' + publicKeyBuffer.toString('hex')
 
-  const address = toChecksumAddress(keccak256(publicKey).slice(-20))
-
-  return {
-    publicKey: '0x' + publicKey.toString('hex'),
-    address
+  // Private key was provided, don't include it in the result.
+  if (maybePrivateKey) {
+    return { publicKey, address }
   }
+
+  // Private key was not provided, include it in result.
+  return { privateKey: '0x' + privateKey.toString('hex'), publicKey, address }
 }
 
 module.exports = derive
