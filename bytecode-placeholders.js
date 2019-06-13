369ab641a46687d18c4d89a2216fa56af47315a3
Mirek Rusin <mirek@me.com>
2019-06-13 08:23:32 +0200

    Removing ambiguity test in bytecode-placeholders.
    Solidity changed link placeholders to truncated keccak256 hashes, which
    solves this problem.

--- bytecode-placeholders.js
+++ bytecode-placeholders.js
@@ -1,17 +1,23 @@
+// @flow
 
 const assert = require('assert')
-const { isString, sumBy } = require('lodash')
+const isString = require('lodash/isString')
 
-function bytecodePlaceholders(value) {
+/** @returns placeholders in bytecode. */
+function bytecodePlaceholders(value /*: string */) /*: { [placeholder: string]: {| index: number |}[] } */ {
   assert(isString(value) && value.startsWith('0x'), 'Expected hex0x-like bytecode (string starging with 0x).')
   const re = /__.{36}__/g
-  let p
-  const ps = []
-  while ((p = re.exec(value))) {
-    ps.push({ name: p[0], index: p.index })
+  const placeholders = {}
+  let match
+  while ((match = re.exec(value))) {
+    const [ placeholder ] = match
+    const { index } = match
+    if (!placeholders[placeholder]) {
+      placeholders[placeholder] = []
+    }
+    placeholders[placeholder].push({ index })
   }
-  ps.forEach(p => (p.ambiguous = sumBy(ps, _1 => (_1.name === p.name ? 1 : 0)) > 1))
-  return ps
+  return placeholders
 }
 
 module.exports = bytecodePlaceholders
