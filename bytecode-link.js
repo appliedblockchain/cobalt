1b3fb219b76409b4308b2ec457148196d0edf602
Mirek Rusin <mirek@me.com>
2019-06-13 08:24:24 +0200

    Updating bytecode-link helper.

--- bytecode-link.js
+++ bytecode-link.js
@@ -1,43 +1,43 @@
+// @flow
 
-const { map, keys, difference } = require('lodash')
-const hex0xToHex = require('./hex0x-to-hex')
+const keys = require('lodash/keys')
+const difference = require('lodash/difference')
 const bytecodePlaceholders = require('./bytecode-placeholders')
 const jsonStringify = require('./json-stringify')
+const placeholderOfString = require('./placeholder-of-string')
 
 /**
   * Replace placeholders with provided addresses.
   *
   * @param {string} bytecode Unlinked bytecode.
-  * @param {object} links Placeholder to address mapping.
-  * @return {hex0x} Linked bytecode.
+  * @param addressesOfPaths Mapping from file paths to ethereum addresses.
+  * @returns linked bytecode.
   */
-function bytecodeLink(bytecode, links = {}) {
+function bytecodeLink(bytecode /*: string */, addressesOfPaths /*: { [string]: string } */ = {}) /*: string */ {
+
+  // Construct mapping from placeholder to path and address.
+  const links = keys(addressesOfPaths)
+    .map(path => ({ path, placeholder: placeholderOfString(path), address: addressesOfPaths[path] }))
+    .reduce((_, { path, placeholder, address }) => ({ ..._, [placeholder]: { path, address } }), {})
+
+  const placeholders = bytecodePlaceholders(bytecode)
 
   // Collect keys so we can check if there's one-to-one match.
   const as = keys(links)
-  const bs = map(bytecodePlaceholders(bytecode), 'name')
+  const bs = keys(placeholders)
 
-  // Check for extra links provided.
-  {
-    const diff = difference(as, bs)
-    if (diff.length) {
-      throw new Error(`Unnecessary extra links provided ${diff.map(jsonStringify).join(', ')}.`)
-    }
+  const abDiff = difference(as, bs)
+  const baDiff = difference(bs, as)
+
+  if (abDiff.length || baDiff.length) {
+    throw new Error([
+      abDiff.length ? `Unnecessary extra links provided ${abDiff.map(jsonStringify).join(', ')}.` : null,
+      baDiff.length ? `Missing links for ${baDiff.map(jsonStringify).join(', ')}. Did you forget to provide those links when deploying?` : null
+    ].filter(Boolean).join(' '))
   }
 
-  // Check for missing links.
-  {
-    const diff = difference(bs, as)
-    if (diff.length) {
-      throw new Error(`Missing links for ${diff.map(jsonStringify).join(', ')}. Did you forget to provide those links when deploying?`)
-    }
-  }
-
-  return keys(links).reduce((r, k) =>
-    k.endsWith('___') ?
-      r.split(k).join(hex0xToHex(links[k])) :
-      r.replace(k, hex0xToHex(links[k]))
-    , bytecode)
+  return keys(placeholders)
+    .reduce((_, placeholder) => _.split(placeholder).join(links[placeholder].address.slice(2)), bytecode)
 }
 
 module.exports = bytecodeLink
